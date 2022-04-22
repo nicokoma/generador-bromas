8187f81dee1080e25a7fcadcdcf695c104836ff3
Nicolás Komarosvky / Web Designer <103187054+nicokoma@users.noreply.github.com>
2022-04-22 11:16:52 -0300

    Update generador.py

--- generador.py
+++ generador.py
@@ -1,8 +1,8 @@
 import random
 
 def main():
-	animales = ["el elefante", "la gallina", "la cotorra", "el toro", "la morsa", "el manati", "la vaca", "la iguana", "el conejo", "el lobo", "el perro", "el gato", "la libre", "el raton", "la chinchilla"]
-	verbos = ["acariciar", "consultar", "trabajar", "animar", "ensuciar", "sacudir", "lustrar", "pulir", "encerar", "desmembrar", "degollar", "aplaudir", "educar", "ayudar"]
+	animales = ["el elefante", "la gallina", "la cotorra", "el toro", "la morsa", "el manati", "la vaca", "la iguana", "el conejo", "el lobo", "el perro", "el gato", "la libre", "el raton", "la chinchilla", "la rata"]
+	verbos = ["acariciar", "consultar", "trabajar", "animar", "ensuciar", "sacudir", "lustrar", "pulir", "encerar", "desmembrar", "degollar", "aplaudir", "educar", "ayudar", "masajear"]
 	
 	for i in range(20):
 		print(random.choice(verbos) + " " + random.choice(animales))
